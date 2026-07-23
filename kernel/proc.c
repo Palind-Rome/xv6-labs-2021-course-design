13f443da5a7f0163f0ec2f7ecd32df0871f7c50c
Palind <46059910+Palind-Rome@users.noreply.github.com>
2026-07-23 19:47:39 +0800

    Complete the syscall lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -144,6 +144,20 @@ found:
   return p;
 }
 
+// get number of NOT UNUSED process
+uint64
+nproc(void) {
+  uint64 cnt = 0;
+  struct proc *p;
+  for (p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if (p->state != UNUSED)
+      cnt++;
+    release(&p->lock);
+  }
+  return cnt;
+}
+
 // free a proc structure and the data hanging from it,
 // including user pages.
 // p->lock must be held.
@@ -302,6 +316,9 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+  
+  // copy parent trace mask
+  np->tracemask = p->tracemask;
 
   pid = np->pid;
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,4 +95,30 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int mask;
+  if (argint(0, &mask) < 0)
+    return -1;
+  myproc()->tracemask = mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 sinfo_addr;
+  if (argaddr(0, &sinfo_addr) < 0)
+    return -1;
+
+  struct sysinfo sinfo;
+  sinfo.freemem = freemem();
+  sinfo.nproc = nproc();
+  struct proc *p = myproc();
+  if (copyout(p->pagetable, sinfo_addr, (char *)&sinfo, sizeof(sinfo)) < 0)
+    return -1;
+  return 0;
 }
